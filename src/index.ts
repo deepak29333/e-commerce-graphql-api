1645696f79f88260d7224618d8fbd0d5dd6e2a37
DEEPAK <deepakkumar29333@gmail.com>
2024-12-17 23:55:15 +0530

    single product with variant pagiantion
    and rest  api server is also created

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,24 @@ import {mergeTypeDefs} from "@graphql-tools/merge";
 import {loadFilesSync} from "@graphql-tools/load-files";
 import {resolvers} from "./resolvers/resolvers";
 import {prisma} from "./db/prismaClient";
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
 const typesArray = loadFilesSync('./src/schema/*.graphql');
 const typeDefs = mergeTypeDefs(typesArray);
 const schema = makeExecutableSchema({typeDefs, resolvers});
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/health', async (ctx) => {
+  ctx.status = 200;
+  ctx.body = 'Project is running';
+});
+
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,4 +31,8 @@ const server = new ApolloServer({
 
 server.listen().then(({url}) => {
   console.log(`🚀 Server ready at ${url}graphql.json`);
+});
+
+app.listen(3000, () => {
+  console.log('Express server running on port 3000');
 });
